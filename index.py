52ac1f5622e57f2f2b1cb61089f2ec2fbb812169
Lokesh Obbina <pratap.pulugoru@tcs.com-at-931096194282>
2021-09-12 23:05:17 +0530

    This is benz car

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 class Car:
-  def __init__(self):
-    print("This is a new Car")
+  def __init__(self,brand_name,model_no):
+    self.brand_name = brand_name
+    self.model_no = model_no
 
   def start_engine(self):
     print("Engine Started")
@@ -9,5 +10,9 @@ class Car:
     print("Engine Stopped")
 
   def get_info(self):
-    print("Model No."+self.model_no)
-    print("Brand Name"+self.brand_name)
+    print("Brand Name: "+self.brand_name)
+    print("Model No.: "+self.model_no)
+
+
+c = Car("Benz","C2")
+c.get_info()
