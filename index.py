d2a08835ec6d281eff6373af697f6dbc72114aa8
Lokesh Obbina <pratap.pulugoru@tcs.com-at-931096194282>
2021-09-12 22:58:16 +0530

    updated new functions

--- index.py
+++ index.py
@@ -1,3 +1,13 @@
 class Car:
   def __init__(self):
     print("This is a new Car")
+
+  def start_engine(self):
+    print("Engine Started")
+
+  def stop_engine(self):
+    print("Engine Stopped")
+
+  def get_info(self):
+    print("Model No."+self.model_no)
+    print("Brand Name"+self.brand_name)
